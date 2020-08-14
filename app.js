071a4d4ede0797e00a8c5c795ff642f66958b439
Jith <jith4j@gmail.com>
2020-08-14 17:56:43 +0530

    added faded navbar in mobile phone

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-const texts = ['Developer. ', 'Programmer. ', 'Techie. ']
+const texts = ["Developer. ", "Programmer. ", "Techie. "];
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let currentText = "";
+let letter = "";
 
 (function type() {
-    if(count === texts.length){
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.querySelector('.typing').textContent = letter;
-    
-    if(letter.length === currentText.length){
-        count++;
-        index = 0;
-    }
+  document.querySelector(".typing").textContent = letter;
 
-    setTimeout(type, 400);
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
 
+  setTimeout(type, 400);
 })();
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.toggle('open');
+    links.forEach((link) => {
+        link.classList.toggle("fade");
+      });
+});
